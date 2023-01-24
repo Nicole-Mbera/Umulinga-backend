54b0ef3da630d81ff7887fbd3c94a71b65d68736
mutuyimana1 <amutuyimana64@gmail.com>
2023-01-24 10:11:41 +0200

    add cors

--- Server/model/product.js
+++ Server/model/product.js
@@ -1,26 +1,19 @@
-
 import mongoose from "mongoose";
 
-
 const productSchema = new mongoose.Schema(
-    {
-        productPicture: [
-            {type: String}
-        ],
-      read:{type: String, default: 'no'},
-      like:{ type: Number, default: 0 },
-        name: String,
-        description: String,
-        price: Number,
-        
-    },
-    {
-        timestamps: true,
-    }
-
-
+  {
+    productPicture: [{ type: String }],
+    read: { type: String, default: "no" },
+    like: { type: Number, default: 0 },
+    name: String,
+    description: String,
+    price: Number,
+  },
+  {
+    timestamps: true,
+  }
 );
-    
+
 const Product = mongoose.model("Product", productSchema);
 
 export default Product;
--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 import mongoose from "mongoose";
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import userRouter from "./Server/routes/userRoutes"
-import productRouter from "./Server/routes/productRoute"
+import userRouter from "./Server/routes/userRoutes";
+import productRouter from "./Server/routes/productRoute";
+import cors from "cors";
 
-dotenv.config ("./.env");
+dotenv.config("./.env");
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use("/user",userRouter);
-app.use("/product", productRouter)
-app.use("/",(req,res)=> res.status(200).json({
-  message: "This is  APi does not exist"
 
+app.use("/user", userRouter);
+app.use("/product", productRouter);
+app.use("/", (req, res) =>
+  res.status(200).json({
+    message: "This is  APi does not exist",
+  })
+);
 
-}));
-
-const databaseUrl=process.env.DATABASE;
+const databaseUrl = process.env.DATABASE;
 mongoose
-  .connect(databaseUrl, {useNewUrlParser:true, useUnifiedTopology:true,})
+  .connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("database conected successfully"));
 
-  const port = process.env.PORT;
-app.listen( port,()=>{
-    console.log(`server is running on port  ${port}`)
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`server is running on port  ${port}`);
+});
 
 export default app;
